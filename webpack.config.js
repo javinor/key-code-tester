c0aac2898bf5c994c9271b3bbc206be33175fc2a
javinor <javinor@gmail.com>
2017-07-03 01:14:36 +0300

    playing with prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
@@ -12,19 +14,51 @@ module.exports = {
   devServer: { contentBase: path.resolve(__dirname, 'docs') },
   module: {
     loaders: [
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
-      { test: /\.less$/, loaders: ['style', 'css', 'less'] },
+      { test: /\.css$/, use: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'}) },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader', query: {presets: ['es2015']} },
+      // { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
+      // { test: /\.less$/, loaders: ['style', 'css', 'less'] },
       { test: /\.woff$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]' },
       { test: /\.woff2$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]' },
       { test: /\.(eot|ttf|svg|gif|png)$/, loader: 'file-loader' }
+    ]
+  },
+  resolve: {
+    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, 'src')
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/main.html',
       filename: 'index.html'
+    }),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {'NODE_ENV': JSON.stringify('production')}
     })
   ]
 }
